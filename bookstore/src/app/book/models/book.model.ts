023173a95af74cbfd503909f4c6d26d22784d17b
gopika <gopikalakshmia@gmail.com>
2022-01-09 10:07:53 -0700

    added book model,book-card

---
+++ bookstore/src/app/book/models/book.model.ts
@@ -0,0 +1,8 @@
+export interface BookModel{
+    id:number;
+    name:string;
+    author:string;
+    price:number;
+    imgSrc:string;
+    
+}
--- bookstore/src/app/home/home.module.ts
+++ bookstore/src/app/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from '../shared/shared.module';
-
+import { BookModule } from '../book/book.module';
 
 
 @NgModule({
@@ -11,7 +11,8 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    BookModule
   ],
   exports:[HomeComponent]
 })
